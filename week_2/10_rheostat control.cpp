f8ea6e36e558b2d1a7e7fa2038595a9be9afeb2f
Yejin Cho <80540445+yejinj@users.noreply.github.com>
2023-01-18 16:48:22 +0900

    Create 10_rheostat control.cpp

---
+++ week_2/10_rheostat control.cpp
@@ -0,0 +1,16 @@
+int potPin = 0;
+int ledPin = 11;
+int potValue = 0;
+int ledValue = 0;
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    potValue = analogRead(potPin);
+    brightness = map(potValue, 0, 1023, 0, 255);
+
+    digitalWrite(ledPin, brightness);
+    delay(100);
+}
