e96004937006a03739945bf76000c35f85952762
Yejin Cho <80540445+yejinj@users.noreply.github.com>
2023-01-18 19:47:09 +0900

    Update 9_light_censor_control.cpp

--- week_2/9_light_censor_control.cpp
+++ week_2/9_light_censor_control.cpp
@@ -13,3 +13,41 @@ void loop() {
     digitalWrite(ledPin, ledValue);
     delay(10);
 }
+
+
+//조도센서로 주변 밝기 측정하기
+int cdsPin = 0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int value = analogRead(cdsPin);
+  Serial.print("value: ");
+  Serial.println(value);
+  delay(200);
+}
+
+// 조도 센서로 LED 깜박이기
+int redPin = 13;
+int greenPin = 11;
+int cdsPin = 0;
+int cdsValue = 0;
+
+void setup(){
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+}
+
+void loop(){
+  cdsValue = analogRead(cdsPin);
+
+  if(cdsValue < 600){
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  } else{
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }
+}
